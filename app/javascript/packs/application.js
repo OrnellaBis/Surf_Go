32d23265b6815d804b196d360527214003d59664
Ornella Bissessur <ornella.bissessur@gmail.com>
2021-08-25 15:19:28 +0200

    change devise pages controller

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,12 +24,12 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import {fetchMethode} from "../components/_API-Storm.js";
+// import {fetchMethode} from "../components/_API-Storm.js";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  fetchMethode();
+ //fetchMethode();
 });
