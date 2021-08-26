16d7e33d0f43bb464f1d2323feba7b702687fca9
benoit jeanjoseph <benoit.jeanjoseph@gmail.com>
2021-08-26 03:13:44 +0200

    ajpout photos

--- app/javascript/components/toggle_add_forecast.js
+++ app/javascript/components/toggle_add_forecast.js
@@ -8,18 +8,22 @@ const btn = document.getElementById("myBtn");
 const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.addEventListener("click",  ()=>{ 
+    console.log("HELOOOOOOOOO")
   modal.style.display = "block";
-}
+})
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+span.addEventListener("click",  ()=>{ 
+    modal.style.display = "none";
+})
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+  
 }
+window.addEventListener("click",  (event)=>{ 
+    if (event.target == modal) {
+        modal.style.display = "none";
+      }
+})
