eb1edd7205249d930c62279473bacfd001d0c076
benoit jeanjoseph <benoit.jeanjoseph@gmail.com>
2021-08-28 15:12:06 +0200

    algo

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -54,6 +54,7 @@ class SpotsController < ApplicationController
         lng: spot.longitude
       }
     end
+    
   end
 
   def show
@@ -63,20 +64,7 @@ class SpotsController < ApplicationController
 
     @air_temperature = @forecast.air_temperature
     @cloud_cover = @forecast.cloud_cover
-    @current_direction = @forecast.current_direction
-    @current_speed = @forecast.current_speed
-    @swell_direction = @forecast.swell_direction
-    @swell_height = @forecast.swell_height
-    @water_temperature = @forecast.water_temperature
-    @wave_direction = @forecast.wave_direction
     @wave_height = @forecast.wave_height
-    @wave_period = @forecast.wave_period
-    @wind_wave_height = @forecast.wind_wave_height
-    @wind_direction = @forecast.wind_direction
-    @wind_speed = @forecast.wind_speed
-    @gust = @forecast.gust
-    @precipitation = @forecast.precipitation
-    @uv_index = @forecast.uv_index
 
     @minimal_forecats = [@air_temperature, @cloud_cover, @wave_height]
     
@@ -96,6 +84,60 @@ class SpotsController < ApplicationController
       precipitation: @forecast.precipitation,
       uv_index: @forecast.uv_index
     }
-
+    @conditions = surffing_condition(@all_forecast)
   end
+
+  private
+
+  def surffing_condition(forecasts)
+    mark = 0
+    forecasts.each do |key, value|
+      if key.to_s.include?("wind_direction")
+        if (value > 30 && value < 120)
+          mark += 5
+        elsif (value > 180 && value < 360)
+          mark -= 5
+        end
+        
+      end
+      if key.to_s.include?("speed")
+        if key.to_s.include?("wind")
+          if (value < 3)
+            mark += 5
+          elsif value > 4
+            mark -= 2
+          end
+        end
+        #ajouter les données de courent
+      end
+      if key.to_s.include?("wave_height")
+        if(value > 2)
+          mark += 10
+        elsif (value > 1)
+          mark += 5
+        elsif (value > 0.3)
+          mark += 1
+        end
+      end
+      if key.to_s.include?("periode")
+        if value > 11 && value <= 12
+          mark -= 1
+        elsif value > 8 && value <= 12
+          mark += 3
+        elsif value > 3 && value <= 12
+          mark -= 2
+        end
+        
+      end
+    end
+
+    if mark > 15
+      return "Sorts ta planch mec! Ya des bombes today"
+    elsif mark >= 10
+      return "Bonne journée en prévision, tu vas t'amuser"
+    elsif mark < 10
+      return "Pas top today, mais va voir on sait jamais"
+    end
+  end
+  
 end
