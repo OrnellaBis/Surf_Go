d0d73586308673d77ae37f5511661c0088b883f8
benoit jeanjoseph <benoit.jeanjoseph@gmail.com>
2021-08-28 14:12:18 +0200

    units

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,5 +1,39 @@
 class SpotsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
+  def initialize
+    @units = {
+      current_direction: "Direction du courant",
+      current_speed: "m/s",
+      swell_direction: "Direction de la houle",
+      swell_height: "m",
+      water_temperature: "°C",
+      wave_direction: "Direction des vagues",
+      wave_period: "s",
+      wind_wave_height: "m",
+      wind_direction: "Direction du vent",
+      wind_speed: "m/s",
+      gust: "m/s",
+      precipitation: "Précipitations",
+      uv_index: "Index UV",
+    }
+
+    @label_forecast = {
+      current_direction: "Direction du courant",
+      current_speed: "Vitesse du courant",
+      swell_direction: "Direction de la houle",
+      swell_height: "Hauteur de la houle",
+      water_temperature: "Température de l'eau",
+      wave_direction: "Direction des vagues",
+      wave_period: "Période entre les vagues",
+      wind_wave_height: "Hauteur des vagues en fonction du vent",
+      wind_direction: "Direction du vent",
+      wind_speed: "Vitesse des vents",
+      gust: "Rafales",
+      precipitation: "Précipitations",
+      uv_index: "Index UV",
+    }
+  end
+  
 
   def index
     if params[:query].present?
@@ -39,36 +73,22 @@ class SpotsController < ApplicationController
     @uv_index = @forecast.uv_index
 
     @minimal_forecats = [@air_temperature, @cloud_cover, @wave_height]
-    @label_forecast = {
-      current_direction: "Direction du courant",
-      current_speed: "Vitesse du courant",
-      swell_direction: "Direction de la houle",
-      swell_height: "Hauteur de la houle",
-      water_temperature: "Température de l'eau",
-      wave_direction: "Direction des vagues",
-      wave_period: "Période entre les vagues",
-      wind_wave_height: "Hauteur des vagues en fonction du vent",
-      wind_direction: "Direction du vent",
-      wind_speed: "Vitesse des vents",
-      gust: "Rafales",
-      precipitation: "Précipitations",
-      uv_index: "Index UV",
-    }
+    
     @all_forecast =
       {
-      current_direction: @current_direction,
-      current_speed: @current_speed,
-      swell_direction: @swell_direction,
-      swell_height: @swell_height,
-      water_temperature: @water_temperature,
-      wave_direction: @wave_direction,
-      wave_period: @wave_period,
-      wind_wave_height: @wind_wave_height,
-      wind_direction: @wind_direction,
-      wind_speed: @wind_speed,
-      gust: @gust,
-      precipitation: @precipitation,
-      uv_index: @uv_index
+      current_direction: @forecast.current_direction,
+      current_speed: @forecast.current_speed,
+      swell_direction:@forecast.swell_direction,
+      swell_height: @forecast.swell_height,
+      water_temperature: @forecast.water_temperature,
+      wave_direction: @forecast.wave_direction,
+      wave_period: @forecast.wave_period,
+      wind_wave_height: @forecast.wind_wave_height,
+      wind_direction: @forecast.wind_direction,
+      wind_speed: @forecast.wind_speed,
+      gust: @forecast.gust,
+      precipitation: @forecast.precipitation,
+      uv_index: @forecast.uv_index
     }
 
   end
