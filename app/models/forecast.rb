5800fe9ef7e6ac84a851f81af892e64785fc8d8f
benoit jeanjoseph <benoit.jeanjoseph@gmail.com>
2021-08-31 16:39:49 +0200

    many val + modals

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,5 +1,13 @@
 class Forecast < ApplicationRecord
   belongs_to :spot
-  has_many :validation_forcasts
+  has_many :validation_forecasts
 
+  def is_validate?
+    if self.validation_forecasts.size >= 5
+      return true
+    else
+      return false
+    end
+  end
+  
 end
--- app/models/validation_forecast.rb
+++ app/models/validation_forecast.rb
@@ -3,4 +3,5 @@ class ValidationForecast < ApplicationRecord
   belongs_to :user
 
   validates_uniqueness_of :forecast, scope: [:user_id]
+  
 end
