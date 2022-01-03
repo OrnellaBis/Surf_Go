30ea8efa8b2d5068e19d3485470c8e07b16242aa
Ornella Bissessur <ornella.bissessur@gmail.com>
2022-01-03 16:04:42 +0100

    precompile add

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module SurfGo
     config.load_defaults 6.0
     config.assets.initialize_on_precompile = false
 
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
